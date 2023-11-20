67b517629226e64157e068d4b4a821f2f4b93ead
pcaldi <pcaldi@gmail.com>
2023-11-20 20:24:18 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
+      <h1 className="font-extrabold text-5xl mb-4">Hello Tailwind</h1>
+      <h2>Hello Tailwind</h2>
+      <p>Hello Tailwind</p>
+    </div>
+  )
+}
