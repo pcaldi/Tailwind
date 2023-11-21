cabf70364dfcc4f6ee1e54cd4b1a2e0871f0d032
pcaldi <pcaldi@gmail.com>
2023-11-20 21:08:36 -0300

    responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-extrabold flex items-center text-5xl mb-4 before:m-1 before:h-8 before:w-1 before:bg-sky-500 before:flex">Hello Tailwind</h1>
+      <h1 className="font-extrabold flex items-center text-3xl sm:text-5xl lg:text-6xl mb-4 before:m-1 before:h-8 before:w-0.5 gap-2 before:bg-sky-500 before:flex">Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
       <button disabled className="bg-sky-400 font-medium text-lg rounded-md px-4 py-2 mt-4 enabled:hover:bg-sky-500 disabled:opacity-60 disabled:cursor-not-allowed" >
