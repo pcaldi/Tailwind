d37bac30fcbcedc42f40c58c99168d4bafa81b25
pcaldi <pcaldi@gmail.com>
2023-12-12 13:17:21 -0300

    create FileList

---
+++ src/components/Form/FileInput/FileList.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { FileItem } from './FileItem'
+import { useFileInput } from './Root'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+
+export function FileList() {
+  const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
+
+  return (
+    <div ref={parent} className="mt-4 flex flex-col gap-3">
+      {files.map((file) => {
+        return (
+          <FileItem
+            key={file.name}
+            name={file.name}
+            size={file.size}
+            state="error"
+          />
+        )
+      })}
+    </div>
+  )
+}
